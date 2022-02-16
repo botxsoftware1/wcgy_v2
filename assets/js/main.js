9b20f89d2ab25d4e05a3419496c891fff1fe34f5
haribotx <80144381+haribotx@users.noreply.github.com>
2022-02-16 20:15:04 +0530

    phone nunber pattern added

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,7 +196,7 @@ const apply = (event) => {
 
                   <div>
                       <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your WhatsApp Number</label>
-                      <input type="tel" name="phone" id="email" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="999XX909XX" required>
+                      <input type="tel" name="phone" id="phone" pattern="[7-9]{1}[0-9]{9}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="999XX909XX" required>
                   </div>
 
                   <div>
