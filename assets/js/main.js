b21b46cf1dfe24d2f2b440ce511a2fe1b8b6a739
haribotx <80144381+haribotx@users.noreply.github.com>
2022-02-18 17:56:39 +0530

    jio

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,6 +219,6 @@ var i;
 var divs = document.getElementById('myl');
 for(i=0;i<divs.length;i++) {
   if(divs[i].className == 'myclass') {
-    divs[i].innerHTML = divs[i].innerHTML.substring(0,10);
+    divs[i].innerHTML = divs[i].innerHTML.substring(0,3);
   }
 }
